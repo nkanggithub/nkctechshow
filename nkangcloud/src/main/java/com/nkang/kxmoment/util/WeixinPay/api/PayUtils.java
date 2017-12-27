dbb76ba8c586668374318b8b4462b616e632350a
nkanggithub <camning_amanda@hotmail.com>
2017-12-27 14:19:48 +0800

    saved

---
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/WeixinPay/api/PayUtils.java
@@ -0,0 +1,136 @@
+package com.nkang.kxmoment.util.WeixinPay.api;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.nkang.kxmoment.util.WeixinPay.bean.PayCallbackNotify;
+import com.nkang.kxmoment.util.WeixinPay.bean.PayNativeInput;
+import com.nkang.kxmoment.util.WeixinPay.bean.PayPackage;
+import com.nkang.kxmoment.util.WeixinPay.bean.PayQrCode;
+import com.nkang.kxmoment.util.WeixinPay.utils.Configure;
+import com.nkang.kxmoment.util.WeixinPay.utils.HttpsRequest;
+import com.nkang.kxmoment.util.WeixinPay.utils.MapUtil;
+import com.nkang.kxmoment.util.WeixinPay.utils.Signature;
+
+/**
+ * 
+ * @author louiseliu
+ *
+ */
+public class PayUtils {
+	private static final Logger logger = Logger.getLogger(PayUtils.class);
+	 
+	public static String generateMchPayNativeRequestURL(String productid){
+		PayQrCode qrCode = new PayQrCode(productid);
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("sign", qrCode.getSign());
+		map.put("appid", qrCode.getAppid());
+		map.put("mch_id", qrCode.getMch_id());
+		map.put("product_id", qrCode.getProduct_id());
+		map.put("time_stamp", qrCode.getTime_stamp());
+		map.put("nonce_str", qrCode.getNonce_str());
+		
+/*		map.put("body", "LeshuCourse");
+		map.put("device_info", "WEB");
+		map.put("notify_url", "http://leshucq.bceapp.com/mdm/AddQuestions.jsp");
+		map.put("out_trade_no", "123456");
+		map.put("sign_type", "MD5");
+		map.put("total_fee", "1");
+		map.put("trade_type", "JSAPI");*/
+		
+		String xmlStr = "<xml><appid>"+qrCode.getAppid()+"</appid><body>LeshuCourse</body><device_info>WEB</device_info><mch_id>"+qrCode.getMch_id()+"</mch_id><nonce_str>123</nonce_str><notify_url>http://leshucq.bceapp.com/mdm/AddQuestions.jsp</notify_url><out_trade_no>111</out_trade_no><sign_type>MD5</sign_type><total_fee>1</total_fee><trade_type>JSAPI</trade_type><openid>oqPI_xLq1YEJOczHi4DS2-1U0zqc</openid><sign>"+qrCode.getSign()+"</sign></xml>";
+		return xmlStr;
+		//return "weixin://wxpay/bizpayurl?" + MapUtil.mapJoin(map, false, false);
+	}
+	
+	/**
+	 * 
+	 * @param inputStream request.getInputStream()
+	 * @return
+	 */
+	public static PayNativeInput convertRequest(InputStream inputStream){
+		try {
+			String content = IOUtils.toString(inputStream);
+			
+			XmlMapper xmlMapper = new XmlMapper();
+			PayNativeInput payNativeInput = xmlMapper.readValue(content, PayNativeInput.class);
+			
+			return payNativeInput;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static boolean validateAppSignature(PayNativeInput payNativeInput){
+		try {
+			Map<String, String> map = BeanUtils.describe(payNativeInput);
+			map.remove("class");
+			map.put("sign", "");
+			
+			String sign = Signature.generateSign(map);
+			return payNativeInput.getSign().equals(sign) ? true : false;
+		} catch (Exception e) {
+		}
+		
+		return false;
+	}
+	
+	public static String generatePayNativeReplyXML(PayPackage payPackage){
+		try {
+			
+			Map<String, String> map = BeanUtils.describe(payPackage);
+			map.remove("class");
+			
+			String sign = Signature.generateSign(map);
+			payPackage.setSign(sign);
+			
+			XmlMapper xmlMapper = new XmlMapper();
+			xmlMapper.setSerializationInclusion(Include.NON_EMPTY);
+			
+			String xmlContent = xmlMapper.writeValueAsString(payPackage);
+			
+			HttpsRequest httpsRequest = new HttpsRequest();
+			String result = httpsRequest.sendPost(Configure.UNIFY_PAY_API, xmlContent);
+			return result;
+		} catch (Exception e) {
+			logger.info("e:" + e);
+		}
+		
+		return null;
+	}
+	
+	public static PayCallbackNotify payCallbackNotify(InputStream inputStream){
+		try {
+			String content = IOUtils.toString(inputStream);
+			
+			XmlMapper xmlMapper = new XmlMapper();
+			PayCallbackNotify payCallbackNotify = xmlMapper.readValue(content, PayCallbackNotify.class);
+			if(payCallbackNotify.getResult_code().equals("SUCCESS")
+					&& payCallbackNotify.getReturn_code().equals("SUCCESS")){
+				payCallbackNotify.setPaySuccess(true);
+			}
+			return payCallbackNotify;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public static String generatePaySuccessReplyXML(){
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("<xml>")
+					.append("<return_code><![CDATA[SUCCESS]]></return_code>")
+					.append("<return_msg><![CDATA[OK]]></return_msg>")
+					.append("</xml>");
+		return stringBuffer.toString();
+	}
+}
---
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/WeixinPay/bean/PayQrCode.java
@@ -0,0 +1,175 @@
+package com.nkang.kxmoment.util.WeixinPay.bean;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import com.nkang.kxmoment.util.WeixinPay.utils.Configure;
+import com.nkang.kxmoment.util.WeixinPay.utils.MapUtil;
+import com.nkang.kxmoment.util.WeixinPay.utils.Signature;
+
+
+/**
+ * 
+ * @author louiseliu
+ *
+ */
+public class PayQrCode {
+
+	private String appid = "";
+	private String mch_id = "";
+	private String time_stamp ="";
+	private String nonce_str ="";
+	private String product_id = "";
+	private String sign = "";
+	private String body = "";
+	private String device_info = "";
+	private String notify_url = "";
+	private String out_trade_no = "";
+	private String sign_type = "";
+	private String total_fee = "";
+	private String trade_type = "";
+	private String openid = "";
+	
+	public String getOpenid() {
+		return openid;
+	}
+
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getDevice_info() {
+		return device_info;
+	}
+
+	public void setDevice_info(String device_info) {
+		this.device_info = device_info;
+	}
+
+	public String getNotify_url() {
+		return notify_url;
+	}
+
+	public void setNotify_url(String notify_url) {
+		this.notify_url = notify_url;
+	}
+
+	public String getOut_trade_no() {
+		return out_trade_no;
+	}
+
+	public void setOut_trade_no(String out_trade_no) {
+		this.out_trade_no = out_trade_no;
+	}
+
+	public String getSign_type() {
+		return sign_type;
+	}
+
+	public void setSign_type(String sign_type) {
+		this.sign_type = sign_type;
+	}
+
+	public String getTotal_fee() {
+		return total_fee;
+	}
+
+	public void setTotal_fee(String total_fee) {
+		this.total_fee = total_fee;
+	}
+
+	public String getTrade_type() {
+		return trade_type;
+	}
+
+	public void setTrade_type(String trade_type) {
+		this.trade_type = trade_type;
+	}
+
+	/**
+	 * @param product_id
+	 */
+	public PayQrCode(String product_id){
+		setAppid(Configure.getAppid());
+		setMch_id(Configure.getMchid());
+		//setTime_stamp(System.currentTimeMillis()/1000+"");
+		//setNonce_str(UUID.randomUUID().toString().replace("-", ""));
+		setNonce_str("123");
+		//setProduct_id(product_id);
+		setBody("LeshuCourse");
+		setDevice_info("WEB");
+		setNotify_url("http://leshucq.bceapp.com/mdm/AddQuestions.jsp");
+		setOut_trade_no("111");
+		setSign_type("MD5");
+		setTotal_fee("1");
+		setTrade_type("JSAPI");
+		setOpenid("oqPI_xLq1YEJOczHi4DS2-1U0zqc");
+		try {
+			Map<String, String> map = BeanUtils.describe(this);
+			map.remove("class");
+			
+			String sign = Signature.generateSign(map);
+	        setSign(sign);
+		} catch (Exception e) {
+		}
+	}
+	
+	public String getAppid() {
+		return appid;
+	}
+
+	public void setAppid(String appid) {
+		this.appid = appid;
+	}
+
+	public String getMch_id() {
+		return mch_id;
+	}
+
+	public void setMch_id(String mch_id) {
+		this.mch_id = mch_id;
+	}
+
+	public String getTime_stamp() {
+		return time_stamp;
+	}
+
+	public void setTime_stamp(String time_stamp) {
+		this.time_stamp = time_stamp;
+	}
+
+	public String getNonce_str() {
+		return nonce_str;
+	}
+
+	public void setNonce_str(String nonce_str) {
+		this.nonce_str = nonce_str;
+	}
+
+	public String getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(String product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+}
+
