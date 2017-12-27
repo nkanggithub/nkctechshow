ec9a6cefec23cd437f09279366d175d03e218090
nkanggithub <camning_amanda@hotmail.com>
2017-12-27 14:33:08 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/test/testMain.java
+++ nkangcloud/src/main/java/com/nkang/test/testMain.java
@@ -9,6 +9,9 @@ import com.nkang.kxmoment.util.Constants;
 
 
 
+import com.nkang.kxmoment.util.WeixinPay.api.PayUtils;
+import com.nkang.kxmoment.util.WeixinPay.utils.HttpsRequest;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -16,10 +19,10 @@ import java.net.URL;
 public class testMain {
 
     public static void main(String[] args) throws Exception {
-/*    	String b = PayUtils.generateMchPayNativeRequestURL("1482792242");
+    	String b = PayUtils.generateMchPayNativeRequestURL("1469932302");
     	HttpsRequest req =  new HttpsRequest();
     	String a = req.sendPost("https://api.mch.weixin.qq.com/pay/unifiedorder", b); //POST发送到统一支付接口
-    	System.out.println(a);*/
+    	System.out.println(a);
     }
 
 }
