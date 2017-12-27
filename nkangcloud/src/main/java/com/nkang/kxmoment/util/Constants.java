b0feb769084c17cb12d6e29e662779189ec110dc
nkanggithub <camning_amanda@hotmail.com>
2017-12-27 11:30:55 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/util/Constants.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/Constants.java
@@ -2,20 +2,20 @@ package com.nkang.kxmoment.util;
 
 public final class Constants {
 	private Constants(){}
-	public static final String WECHAT_TOKEN = "9066589d8155cfd6a83044547bbd37c8";
-	public static final String APP_ID = "wxe542e71449270554";
-	public static final String APPSECRET = "e90098f1abd35f8db1ce9a124c80a840";
+	public static final String WECHAT_TOKEN = "SDFCTOKENNKCNKANG";
+	public static final String APP_ID = "wx022b55e5bdea4c51";
+	public static final String APPSECRET = "d3d79cc28df35c18bd192761c77ddd74";
 /*	leshu config*/
-	public static final String BAIDU_APPKEY = "8025350dc1c143b9a52816fd8edf994a";
-	public static final String BAIDU_APPSECRET  = "c3868fd3d2874e0abf05ec4936659bb7";
+	public static final String BAIDU_APPKEY = "92a801f6881743d1b222c58b7dbe2475";
+	public static final String BAIDU_APPSECRET  = "6d7fb5881a9b430596cc915d22ed7241";
 	public static final String GOOGLE_KEY = "AIzaSyB3bZ1N7XRiQu2M4ZB3QQMxd3VVLnzrRK4";
 	public static final String proxyInfo = "web-proxy.austin.hpecorp.net";
-	public static final String baehost = "leshucq.bceapp.com";
-	public static final String clientCode = "LES";
+	public static final String baehost = "nkctech.duapp.com";
+	public static final String clientCode = "NKC";
 	public static final String wechatapihost = "api.weixin.qq.com";
 	public static final String baiduapihost = "api.map.baidu.com";
-	public static final String bucketName = "leshucq";
-	public static final String bosDomain = "leshucq.bj.bcebos.com";
+	public static final String bucketName = "nkctech";
+	public static final String bosDomain = "nkctech.gz.bcebos.com";
 	
 	
 	public static final String ucpass_accountSid = "e332d0755b9060b20ecb53a244ec7015";
