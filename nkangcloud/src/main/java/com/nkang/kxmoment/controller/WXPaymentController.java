1a09949819558549034099f845b69a69c94ed9ac
nkanggithub <camning_amanda@hotmail.com>
2017-12-28 17:16:42 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/controller/WXPaymentController.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/controller/WXPaymentController.java
@@ -26,78 +26,36 @@ public class WXPaymentController {
 	@RequestMapping("/pay/payparm")  
     public void payparm(HttpServletRequest request, HttpServletResponse response){  
         try {  
-/*
-        	
-        	String openId = Constants.devOpenID;
-            String appid = Constants.APP_ID;
-            String paternerKey = Constants.partnerKey; 
-              
-            String out_trade_no = "111"; 
-            Map<String, String> paraMap = new HashMap<String, String>();  
-            paraMap.put("appid", appid);  
-            paraMap.put("attach", "测试");  
-            paraMap.put("body", "测试购买支付");  
-            paraMap.put("mch_id", "1469932302");  
-            paraMap.put("nonce_str", "123");  
-            paraMap.put("openid", openId);  
-            paraMap.put("out_trade_no", out_trade_no);  
-            //paraMap.put("spbill_create_ip", getAddrIp(request));  
-            paraMap.put("total_fee", "1");  
-            paraMap.put("trade_type", "JSAPI");  
-            paraMap.put("notify_url", "http://nkctech.duapp.com/mdm/AddQuestions.jsp");// 此路径是微信服务器调用支付结果通知路径  
-            String sign = getSign(paraMap, paternerKey);  
-            paraMap.put("sign", sign);  
-            // 统一下单 https://api.mch.weixin.qq.com/pay/unifiedorder  
-            String url = "https://api.mch.weixin.qq.com/pay/unifiedorder";  
-  */
+
 	    	String b = PayUtils.generateMchPayNativeRequestURL(Constants.prodID);
 	    	HttpsRequest req =  new HttpsRequest();
 	    	String xmlStr = req.sendPost("https://api.mch.weixin.qq.com/pay/unifiedorder", b);
 	    	log.info("---XMLString From payment request---" + xmlStr);
 
-
-/*            String xml = ArrayToXml(paraMap);  */
-  
-            /*String xmlStr = HttpKit.post(url, xml);  */
-  
-            // 预付商品id
     		String[] a =xmlStr.split("prepay_id");
     		String aa =  a[1].substring(10);
     		String prepay_id = aa.substring(0,aa.length()-5);
 
-  
-/*            if (xmlStr.indexOf("SUCCESS") != -1) {  
-                Map<String, String> map = doXMLParse(xmlStr);  
-                prepay_id = (String) map.get("prepay_id");  
-            }  */
-
-            String timeStamp = "12345";  
+    		String timeStamp = "12345";  
             String nonceStr = "12345";
             Map<String, String> payMap = new HashMap<String, String>();  
-/*            payMap.put("appId", Constants.APP_ID);  
-            payMap.put("timeStamp", timeStamp);  
-            payMap.put("nonceStr", nonceStr);  
-            payMap.put("signType", "MD5");  
-            payMap.put("package", "prepay_id=" + prepay_id); */ 
-            
+
             payMap.put("appId", Constants.APP_ID);  
             payMap.put("timeStamp", timeStamp);  
             payMap.put("nonceStr", nonceStr);  
-            payMap.put("partnerid", "1469932302");  
-            payMap.put("prepayid", prepay_id);
-            payMap.put("package", "Sign=" + "WXPay"); 
-            
+            payMap.put("signType", "MD5");  
+            payMap.put("package", "prepay_id=" + prepay_id);
             
             String paySign = Signature.generateSign(payMap);  
-            payMap.put("pg", prepay_id);  
-            payMap.put("paySign", paySign);  
+            payMap.put("pg", "prepay_id=" + prepay_id);
+            payMap.put("paySign", paySign);
               
             // 拼接并返回json  
             StringBuilder sBuilder = new StringBuilder("[{");  
             sBuilder.append("appId:'").append(Constants.APP_ID).append("',")  
                         .append("timeStamp:'").append(timeStamp).append("',")  
                         .append("nonceStr:'").append(nonceStr).append("',")  
-                        .append("pg:'").append(prepay_id).append("',")  
+                        .append("pg:'").append("prepay_id=" + prepay_id).append("',")  
                         .append("signType:'MD5',")  
                         .append("paySign:'").append(paySign).append("'");  
             sBuilder.append("}]");  
@@ -107,8 +65,6 @@ public class WXPaymentController {
             
         } catch (Exception e) {  
             e.printStackTrace();  
-            log.info("---Ii am here---3:" + e.toString());  
         }
-        log.info("---PaymentController has no problem");
     }
 }
