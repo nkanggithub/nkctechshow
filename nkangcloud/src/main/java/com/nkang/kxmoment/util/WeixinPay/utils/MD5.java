a78a00e99533a6ee7561644e1a7b9669a56ad50e
nkanggithub <camning_amanda@hotmail.com>
2017-12-29 10:58:53 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/util/WeixinPay/utils/MD5.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/WeixinPay/utils/MD5.java
@@ -4,18 +4,14 @@ import java.security.MessageDigest;
 
 /**
  * 
- * @author louiseliu
+ * @author Ning Kang
  *
  */
 public class MD5 {
     private final static String[] hexDigits = {"0", "1", "2", "3", "4", "5", "6", "7",
             "8", "9", "a", "b", "c", "d", "e", "f"};
 
-    /**
-     * è½¬æ�¢å­—èŠ‚æ•°ç»„ä¸º16è¿›åˆ¶å­—ä¸²
-     * @param b å­—èŠ‚æ•°ç»„
-     * @return 16è¿›åˆ¶å­—ä¸²
-     */
+
     public static String byteArrayToHexString(byte[] b) {
         StringBuilder resultSb = new StringBuilder();
         for (byte aB : b) {
@@ -24,11 +20,7 @@ public class MD5 {
         return resultSb.toString();
     }
 
-    /**
-     * è½¬æ�¢byteåˆ°16è¿›åˆ¶
-     * @param b è¦�è½¬æ�¢çš„byte
-     * @return 16è¿›åˆ¶æ ¼å¼�
-     */
+
     private static String byteToHexString(byte b) {
         int n = b;
         if (n < 0) {
@@ -39,11 +31,7 @@ public class MD5 {
         return hexDigits[d1] + hexDigits[d2];
     }
 
-    /**
-     * MD5ç¼–ç �
-     * @param origin åŽŸå§‹å­—ç¬¦ä¸²
-     * @return ç»�è¿‡MD5åŠ å¯†ä¹‹å�Žçš„ç»“æžœ
-     */
+
     public static String MD5Encode(String origin) {
         String resultString = null;
         try {
