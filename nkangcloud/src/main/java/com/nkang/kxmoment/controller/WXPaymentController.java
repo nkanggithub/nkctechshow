1698434de7b38b4e991975cc4e09aaa9f1ea9ced
nkanggithub <camning_amanda@hotmail.com>
2018-01-02 11:14:52 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/controller/WXPaymentController.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/controller/WXPaymentController.java
@@ -32,24 +32,23 @@ public class WXPaymentController {
     		PayQrCode qrCode = new PayQrCode(Constants.prodID, out_trade_no, openId,totalfee);
 
     		String xmlStrp = "<xml><appid>"+Constants.APP_ID+"</appid><body>"+Constants.payBody+"</body><device_info>"+Constants.deviceInfo+"</device_info><mch_id>"+qrCode.getMch_id()+"</mch_id><nonce_str>123</nonce_str><notify_url>"+Constants.notifyURL+"</notify_url><out_trade_no>"+out_trade_no+"</out_trade_no><sign_type>"+Constants.signType+"</sign_type><total_fee>"+totalfee+"</total_fee><trade_type>"+Constants.tradeType+"</trade_type><openid>"+openId+"</openid><sign>"+qrCode.getSign()+"</sign></xml>";
-    		
-	    	//String b = PayUtils.generateMchPayNativeRequestURL(Constants.prodID,openId);
+
 	    	HttpsRequest req =  new HttpsRequest();
 	    	String xmlStr = req.sendPost("https://api.mch.weixin.qq.com/pay/unifiedorder", xmlStrp);
-	    	log.info("---XMLString From payment request---" + xmlStrp);
+/*	    	log.info("---XMLString From payment request---" + xmlStrp);*/
 
     		String[] a =xmlStr.split("prepay_id");
     		String aa =  a[1].substring(10);
     		String prepay_id = aa.substring(0,aa.length()-5);
 
-    		String timeStamp = "12345";  
-            String nonceStr = "12345";
+    		String timeStamp = timeStamps;
+            String nonceStr = Constants.WECHAT_TOKEN;
             Map<String, String> payMap = new HashMap<String, String>();  
 
             payMap.put("appId", Constants.APP_ID);  
             payMap.put("timeStamp", timeStamp);  
             payMap.put("nonceStr", nonceStr);  
-            payMap.put("signType", "MD5");  
+            payMap.put("signType", Constants.signType);  
             payMap.put("package", "prepay_id=" + prepay_id);
             
             String paySign = Signature.generateSign(payMap);  
@@ -65,7 +64,7 @@ public class WXPaymentController {
                         .append("signType:'MD5',")  
                         .append("paySign:'").append(paySign).append("'");  
             sBuilder.append("}]");  
-            log.info("---StringPaymentAfter" + sBuilder.toString());
+/*            log.info("---StringPaymentAfter" + sBuilder.toString());*/
             response.getWriter().print(sBuilder.toString());  
             response.getWriter().close();  
             
