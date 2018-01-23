d81b444ad5b0edffafe1a9b808cdaf17f0ec6bd8
nkanggithub <camning_amanda@hotmail.com>
2018-01-23 13:22:49 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/controller/WXPaymentController.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/controller/WXPaymentController.java
@@ -29,28 +29,14 @@ public class WXPaymentController {
     		@RequestParam(value = "openId") String openId,
     		@RequestParam(value = "totalfee") String totalfee){  
         try {  
-        	/*String timeStamps = String.valueOf((System.currentTimeMillis()/1000));//1970年到现在的秒数
-*/        	
         	Date a = new Date();
-        	System.out.println(a+"----"+a.getTime());
         	String timeStamps = String.valueOf(a.getTime());
-
-        	/*System.out.println(convertTime(a.getTime()));*/
-        	
-
     		String out_trade_no = Constants.clientCode + timeStamps;
     		PayQrCode qrCode = new PayQrCode(Constants.prodID, out_trade_no, openId,totalfee);
     		String xmlStrp = "<xml><appid>"+Constants.APP_ID+"</appid><body>"+Constants.payBody+"</body><device_info>"+Constants.deviceInfo+"</device_info><mch_id>"+Constants.mcthID+"</mch_id><nonce_str>123</nonce_str><notify_url>"+Constants.notifyURL+"</notify_url><out_trade_no>"+out_trade_no+"</out_trade_no><sign_type>"+Constants.signType+"</sign_type><total_fee>"+totalfee+"</total_fee><trade_type>"+Constants.tradeType+"</trade_type><openid>"+openId+"</openid><sign>"+qrCode.getSign()+"</sign></xml>";
 	    	HttpsRequest req =  new HttpsRequest();
 	    	String xmlStr = req.sendPost("https://api.mch.weixin.qq.com/pay/unifiedorder", xmlStrp);
-/*	    	log.info("---XMLString From payment request---" + xmlStrp);*/
-
-/*    		String[] a =xmlStr.split("prepay_id");
-    		String aa =  a[1].substring(10);
-    		String prepay_id = aa.substring(0,aa.length()-5);*/
-
 	    	String prepay_id = PayUtil.getXmlPara(xmlStr,"prepay_id");
-    		
     		String timeStamp = timeStamps;
             String nonceStr = "123";
             Map<String, String> payMap = new HashMap<String, String>();  
@@ -73,7 +59,69 @@ public class WXPaymentController {
                         .append("signType:'MD5',")  
                         .append("paySign:'").append(paySign).append("'");  
             sBuilder.append("}]");  
-/*            log.info("---StringPaymentAfter" + sBuilder.toString());*/
+            response.getWriter().print(sBuilder.toString());  
+            response.getWriter().close();
+            
+            //Save payment records to database
+            Payment pay = new Payment();
+            pay.setAppid(Constants.APP_ID);
+            pay.setOpenid(openId);
+            pay.setMch_id(Constants.mcthID);
+            pay.setTotal_fee(totalfee);
+            pay.setTransaction_id("TBD");
+            pay.setResult_code("TBD");
+            pay.setReturn_code("TBD");
+            pay.setOut_trade_no(out_trade_no);
+            pay.setPrepaypkgID(prepay_id);
+            pay.setCreatedDate(timeStamps);
+            MongoDBBasic.savePaymentHistory(pay);
+
+         // TOD - Save the data to the database for further query
+            
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }
+    }
+	
+	
+	@RequestMapping("/pay/payqrparm")  
+    public void payqrparm(HttpServletRequest request, HttpServletResponse response, 
+    		@RequestParam(value = "openId") String openId,
+    		@RequestParam(value = "totalfee") String totalfee){  
+        try {  
+        	Date a = new Date();
+        	String timeStamps = String.valueOf(a.getTime());
+
+    		String out_trade_no = Constants.clientCode + timeStamps;
+    		PayQrCode qrCode = new PayQrCode(Constants.prodID, out_trade_no, openId,totalfee);
+    		String xmlStrp = "<xml><appid>"+Constants.APP_ID+"</appid><body>"+Constants.payBody+"</body><device_info>"+Constants.deviceInfo+"</device_info><mch_id>"+Constants.mcthID+"</mch_id><nonce_str>123</nonce_str><notify_url>"+Constants.notifyURL+"</notify_url><out_trade_no>"+out_trade_no+"</out_trade_no><sign_type>"+Constants.signType+"</sign_type><total_fee>"+totalfee+"</total_fee><trade_type>"+Constants.tradeTypeNative+"</trade_type><openid>"+openId+"</openid><sign>"+qrCode.getSign()+"</sign></xml>";
+	    	HttpsRequest req =  new HttpsRequest();
+	    	String xmlStr = req.sendPost("https://api.mch.weixin.qq.com/pay/unifiedorder", xmlStrp);
+
+	    	String prepay_id = PayUtil.getXmlPara(xmlStr,"prepay_id");
+    		String codeurl = PayUtil.getXmlPara(xmlStr,"code_url");
+    		String timeStamp = timeStamps;
+            String nonceStr = "123";
+            Map<String, String> payMap = new HashMap<String, String>();  
+
+            payMap.put("appId", Constants.APP_ID);  
+            payMap.put("timeStamp", timeStamp);  
+            payMap.put("nonceStr", nonceStr);  
+            payMap.put("signType", Constants.signType);  
+            payMap.put("package", "prepay_id=" + prepay_id);
+            
+            String paySign = Signature.generateSign(payMap);  
+            payMap.put("pg", "prepay_id=" + prepay_id);
+            payMap.put("paySign", paySign);
+            
+            StringBuilder sBuilder = new StringBuilder("[{");  
+            sBuilder.append("appId:'").append(Constants.APP_ID).append("',")  
+                        .append("timeStamp:'").append(timeStamp).append("',")  
+                        .append("nonceStr:'").append(nonceStr).append("',")  
+                        .append("pg:'").append("prepay_id=" + prepay_id).append("',")  
+                        .append("signType:'MD5',")  
+                        .append("paySign:'").append(paySign).append("'");  
+            sBuilder.append("}]");  
             response.getWriter().print(sBuilder.toString());  
             response.getWriter().close();
             
